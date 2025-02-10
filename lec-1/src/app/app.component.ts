f2aabe727b79fcb302d9100ca008a4b5ac5105a5
Naved khan <nomanali50997@gmail.com>
2025-02-10 09:27:31 +0530

    first commit

---
+++ lec-1/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'lec-1';
+  name = "Naved Ali Khan";  
+  x=10;
+  y=39;
+}
