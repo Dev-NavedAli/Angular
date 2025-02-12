c5cb97b54c6563dd13ac46e397fc0e43d05b312f
Naved khan <nomanali50997@gmail.com>
2025-02-12 16:53:53 +0530

    learn signals

--- lec-1/src/app/app.component.ts
+++ lec-1/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { ProfileComponent } from './profile/profile.component';
@@ -12,18 +12,17 @@ import { ProfileComponent } from './profile/profile.component';
 export class AppComponent {
   title = 'lec-1';
 
-  heros = ['Thor', 'hulk', 'tony', 'captain America', 'wanda']
+  count  = signal(10)
+  x= 20;
 
-  students = [
-    { name: 'Naved Ali Khan', age: 25, email: "nomanali50997@gmail.com" },
-    { name: 'Noman Ali Khan', age: 23, email: "noman@gmail.com" },
-    { name: 'Fareed Ali Khan', age: 24, email: "Devnoman@gmail.com" },
-    { name: 'Sarwaz Ali Khan', age: 26, email: "sarwaz@gmail.com" },
-    { name: 'Taqee Ali Khan', age: 19, email: "TaqeeOLp@gmail.com" }
-  ]
+  constructor(){
+    effect(()=>{
+      console.log(this.count());
+    })
+  }
 
-  getName(val: string) {
-    console.log(val);
+  updateValue(){
+    this.count.set(this.count()+1)    
   }
 
 }
