566c857c7b66c482dd6a3ec24770c417c9670bab
Naved khan <nomanali50997@gmail.com>
2025-02-14 15:30:22 +0530

    learn ngif else

--- lec-1/src/app/app.component.ts
+++ lec-1/src/app/app.component.ts
@@ -11,19 +11,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'lec-1';
 
-  show = false
+  login :boolean = false
 
-  students = ['Naved', 'Noman', 'Sam', 'Anas', 'tarik', 'Ashu', 'Faizan']
-
-  studentsData = [
-    { name: 'naved', age: 25, email: 'naved@gmail.com' },
-    { name: 'noman', age: 22, email: 'noman@gmail.com' },
-    { name: 'sam', age: 20, email: 'sam@gmail.com' },
-    { name: 'anas', age: 24, email: 'anas@gmail.com' },
-    { name: 'tarik', age: 26, email: 'tarik@gmail.com' },
-    { name: 'ashu', age: 24, email: 'ashu@gmail.com' },
-    { name: 'Potato', age: 19, email: 'potato@gmail.com' }
-  ]
+  update(){
+    this.login = !this.login
+  }
+  
 
 
 }
