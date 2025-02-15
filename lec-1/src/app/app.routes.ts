c94a9ab51ead5b5d1c852912c751526b30b88cd3
Naved khan <nomanali50997@gmail.com>
2025-02-15 15:57:05 +0530

    making 404 page

--- lec-1/src/app/app.routes.ts
+++ lec-1/src/app/app.routes.ts
@@ -4,12 +4,17 @@ import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+
 
 
 export const routes: Routes = [
+    { path: '', component: HomeComponent },
     { path: 'profile', component: ProfileComponent },
     { path: 'contact', component: ContactComponent },
     { path: 'about', component: AboutComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'signup', component: SignupComponent }
+    { path: 'signup', component: SignupComponent },
+    { path: '**', component:PageNotFoundComponent }
 ];
