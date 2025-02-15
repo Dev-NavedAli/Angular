0623cb9736b96d9ae0b1c33d2875528ee8830daa
Naved khan <nomanali50997@gmail.com>
2025-02-15 13:09:56 +0530

    learn routing with header

--- lec-1/src/app/app.component.ts
+++ lec-1/src/app/app.component.ts
@@ -1,17 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink,RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'lec-1';
-
-
-  
 
 }
--- lec-1/src/app/app.routes.ts
+++ lec-1/src/app/app.routes.ts
@@ -1,12 +1,15 @@
 import { Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
 
 export const routes: Routes = [
-    { path: 'about', component: AboutComponent },
-    { path: 'contact', component: ContactComponent },
     { path: 'profile', component: ProfileComponent },
-    { path: 'login', component: LoginComponent }
+    { path: 'contact', component: ContactComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignupComponent }
 ];
