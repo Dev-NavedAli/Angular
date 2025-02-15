6573528d677d9d60e60fc8f5d43e911d7822d847
Naved khan <nomanali50997@gmail.com>
2025-02-15 11:00:45 +0530

    learn routing

--- lec-1/src/app/app.component.ts
+++ lec-1/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RouterLink,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- lec-1/src/app/app.routes.ts
+++ lec-1/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'profile', component: ProfileComponent },
+    { path: 'login', component: LoginComponent }
+];
