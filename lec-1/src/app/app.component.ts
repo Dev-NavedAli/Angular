9024cc627309383aceba7b39aa2970baf7ca51cf
Naved khan <nomanali50997@gmail.com>
2025-02-17 12:59:34 +0530

    learn dynamic programing

--- lec-1/src/app/app.component.ts
+++ lec-1/src/app/app.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
+import { RouterOutlet ,RouterLink} from '@angular/router';
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterLink,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'lec-1';
 
+  users = [
+    {name:'Naved',id:1,age:24},
+    {name:'Noman',id:2,age:24},
+    {name:'Tarik',id:3,age:24},
+    {name:'Anas',id:4,age:24},
+    {name:'Ashu',id:5,age:24},
+  ]
 }
--- lec-1/src/app/app.routes.ts
+++ lec-1/src/app/app.routes.ts
@@ -1,20 +1,9 @@
 import { Routes } from '@angular/router';
-import { ProfileComponent } from './profile/profile.component';
-import { ContactComponent } from './contact/contact.component';
-import { AboutComponent } from './about/about.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+
 
 
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'profile', component: ProfileComponent,data:{name:'Naved Ali'} },
-    { path: 'contact', component: ContactComponent },
-    { path: 'about', component: AboutComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'signup', component: SignupComponent },
-    { path: '**', component:PageNotFoundComponent }
+    { path: 'user/:id/:name', component: UserComponent }
 ];
