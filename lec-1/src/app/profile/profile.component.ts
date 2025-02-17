65d2b79955c7cbc2108559ec60d8ee11a8e6840e
Naved khan <nomanali50997@gmail.com>
2025-02-17 10:49:32 +0530

    data pass from one component from another using routerLinnk

--- lec-1/src/app/profile/profile.component.ts
+++ lec-1/src/app/profile/profile.component.ts
@@ -1,12 +1,21 @@
 import { Component } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-    selector:"profile",
-    imports:[],
-    templateUrl:"./profile.component.html",
-    styleUrl:"./profile.style.css"
+    selector: "profile",
+    imports: [],
+    templateUrl: "./profile.component.html",
+    styleUrl: "./profile.style.css"
 })
 
-export class ProfileComponent{
+export class ProfileComponent {
+
+    constructor(private route: ActivatedRoute) { }
+
+    userName: string | null = ""
+    ngOnInit() {
+        this.userName = this.route.snapshot.paramMap.get('name')
+        console.log(this.userName);
+    }
 
 }
