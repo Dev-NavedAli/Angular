91617d85f6b1d838553ef9ee015c44a85e915750
Naved khan <nomanali50997@gmail.com>
2025-02-19 16:33:23 +0530

    learn reactive forms

--- lec-1/src/app/app.component.ts
+++ lec-1/src/app/app.component.ts
@@ -1,22 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet ,RouterLink} from '@angular/router';
+import { FormControl, ReactiveFormsModule } from '@angular/forms'
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink,RouterOutlet],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'lec-1';
 
-  users = [
-    {name:'Naved',id:1,age:24},
-    {name:'Noman',id:2,age:24},
-    {name:'Tarik',id:3,age:24},
-    {name:'Anas',id:4,age:24},
-    {name:'Ashu',id:5,age:24},
-  ]
+  name = new FormControl();
+  password = new FormControl();
+
+  displayValue(){
+    console.log(this.name.value);
+    console.log(this.password.value);
+  }
+
 }
