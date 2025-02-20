37f636926ab07ee81e138e92e97eaee05279b49b
Naved khan <nomanali50997@gmail.com>
2025-02-20 11:05:09 +0530

    learn validation

--- lec-1/src/app/app.component.ts
+++ lec-1/src/app/app.component.ts
@@ -1,11 +1,10 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms'
-
-
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -13,20 +12,26 @@ export class AppComponent {
   title = 'lec-1';
 
   profileForm = new FormGroup({
-    name: new FormControl(),
-    password: new FormControl(),
-    email: new FormControl(),
+    name: new FormControl('',[Validators.required]),
+    password: new FormControl('',[Validators.required,Validators.minLength(8)]),
+    email: new FormControl('',[Validators.required,Validators.maxLength(50),Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]),
   })
 
   onSubmit() {
     console.log('Onsubmit Called', this.profileForm.value);
   }
 
-  setValue() {
-    this.profileForm.setValue({
-      name: "Noman",
-      password: "321",
-      email: "noman@50997gmail.com",
-    })
+  get name(){
+    return this.profileForm.get('name')
   }
+
+  get email(){
+    return this.profileForm.get('email')
+  }
+
+  get password(){
+    return this.profileForm.get('password')
+  }
+
+
 }
