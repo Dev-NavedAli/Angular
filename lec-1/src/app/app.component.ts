b7db727cb87f1b5ff3be61fc4beaeecb1907f6b2
Naved khan <nomanali50997@gmail.com>
2025-02-20 12:06:49 +0530

    learn the template driven forms

--- lec-1/src/app/app.component.ts
+++ lec-1/src/app/app.component.ts
@@ -1,37 +1,20 @@
-import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
+import { FormsModule, NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
-  imports: [ReactiveFormsModule,NgIf],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'lec-1';
 
-  profileForm = new FormGroup({
-    name: new FormControl('',[Validators.required]),
-    password: new FormControl('',[Validators.required,Validators.minLength(8)]),
-    email: new FormControl('',[Validators.required,Validators.maxLength(50),Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]),
-  })
-
-  onSubmit() {
-    console.log('Onsubmit Called', this.profileForm.value);
+  userDetails:any
+  addDetails(val: NgForm) {
+    console.log(val);
+    this.userDetails = val;
   }
 
-  get name(){
-    return this.profileForm.get('name')
-  }
-
-  get email(){
-    return this.profileForm.get('email')
-  }
-
-  get password(){
-    return this.profileForm.get('password')
-  }
 
 
 }
