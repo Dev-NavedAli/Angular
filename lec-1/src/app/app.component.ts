51974a198ed9bf9cfc35b381c05a5bdf5b46a216
Naved khan <nomanali50997@gmail.com>
2025-02-22 12:38:48 +0530

    custom pipes

--- lec-1/src/app/app.component.ts
+++ lec-1/src/app/app.component.ts
@@ -1,22 +1,17 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CurrencyConvertorPipe } from './pipe/currency-convertor.pipe';
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [CurrencyConvertorPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-
-  tittle='Naved Ali Khan'
-  tittle2='NAVED ALI KHAN'
-  tittle3='NAVED ALI KHAN'
-
-  date= new Date()
-
-  amount:number=10
+  
+  amount=10
 
 
 }
---
+++ lec-1/src/app/pipe/currency-convertor.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'currencyConvertor'
+})
+export class CurrencyConvertorPipe implements PipeTransform {
+
+  transform(value: number, ...args: number[]): unknown {
+    
+    return value*85;
+  }
+
+}
